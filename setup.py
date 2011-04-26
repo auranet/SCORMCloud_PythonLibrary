77bb03f67faac4289d446de820659aaec0e3caca
Tyler Lesmann <redhatcat@gmail.com>
2011-04-26 08:28:05 -0600

    Simple install script

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='scormcloud',
+    version='1.0',
+    description='Scormcloud',
+    py_modules=['scormcloud'],
+    install_requires=['lxml']
+)
